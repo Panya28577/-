9447addafc60ce873c9c22268a8546bc742b50f1
Panya28577 <107600048+Panya28577@users.noreply.github.com>
2022-07-07 13:51:36 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1657176645,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bullet-sheet0.png",
+		"images/player-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/bot-sheet0.png",
+		"images/bot2-sheet0.png",
+		"images/bg1-sheet0.png",
+		"images/playgame-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/bg3-sheet0.png",
+		"images/box-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
